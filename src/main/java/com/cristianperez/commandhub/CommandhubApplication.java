f1d399a6c98b4711dc7ba74c984ad28c81989410
Cristian Alvarez <cristian.alvarez@bglgroup.co.uk>
2020-01-07 19:57:50 +0000

    First commit

---
+++ src/main/java/com/cristianperez/commandhub/CommandhubApplication.java
@@ -0,0 +1,13 @@
+package com.cristianperez.commandhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristianperez/commandhub/CommandhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianperez.commandhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
